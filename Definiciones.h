63f70256d4d08c2ac7efea28359fa6eb8ebfe6a4
mauricio-rodriguez <42778143+mauricio-rodriguez@users.noreply.github.com>
2019-10-01 01:50:25 -0500

    h

---
+++ Definiciones.h
@@ -0,0 +1,14 @@
+//
+// Created by mauri on 26/09/2019.
+//
+
+#ifndef SORTMETHODS_DEFINICIONES_H
+#define SORTMETHODS_DEFINICIONES_H
+
+#endif //SORTMETHODS_DEFINICIONES_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+typedef std::vector<int>::iterator itr;
--- Sort.h
+++ Sort.h
@@ -6,8 +6,48 @@
 #define SORT_METHODS_SORT_H
 
 
-class Sort {
+#include "Definiciones.h"
+using namespace std;
+template <typename T>
+class Sorter {
+private:
+    vector<T> valores;
+public:
+    Sorter(const vector<T> &valores) : valores(valores) {}
+    void MergeSort(){
+        auto it_begin = valores.begin();
+        auto it_end= valores.end();
+        auto it_mid= next(it_begin, valores.size()/2);
+    }
+    void QuickSort(vector<T> &valores,itr it_begin,itr it_end){
+        if(valores.size()>1)
+        {   auto mid=partition(it_begin,it_end);
+            QuickSort(valores,it_begin,mid-1);
+            QuickSort(valores,mid+1,it_end);
+        }
+    };
+    void Quick(){
+        QuickSort(valores,begin(valores),end(valores)-1);
+    }
 
+    itr partition(itr left,itr right)
+    {
+        auto i=left-1;
+        auto it=left;
+        while(it<right)
+        {
+            if(*it<=*right){
+                ++i;
+                swap(*i,*it);
+            }
+            ++it;
+        }
+        swap(*(i+1),*right);
+        return ++i;
+    }
+
+    void HeapSort();
+    void ShellSort();
 };
 
 
